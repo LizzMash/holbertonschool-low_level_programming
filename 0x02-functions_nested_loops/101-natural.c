c75b526d5670af0113ea4c7b72862e15aa88d0c1
LizzMash <elizabethmashilo@gmail.com>
2022-02-08 15:08:24 +0200

    commit functions

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,13 +1,37 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "main.h"
 /**
- * main - Entry point
+ * main - check the code for Holberton School students.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
-
+	int i, n = 0;
+​
+	int x = 1024;
+​
+	for (i = 0; i < x; i++)
+	{
+		if (((i % 5) == 0) || ((i % 3) == 0))
+		{
+			n = n + i;
+		}
+	}
+	printf("%d\n", n);
 	return (0);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
