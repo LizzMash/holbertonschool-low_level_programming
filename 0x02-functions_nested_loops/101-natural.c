b5a819a0c103b632dc4687d9d887099c0a99cadb
LizzMash <elizabethmashilo@gmail.com>
2022-02-08 15:13:08 +0200

    commit functions

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,9 +7,7 @@
 int main(void)
 {
 	int i, n = 0;
-​
 	int x = 1024;
-​
 	for (i = 0; i < x; i++)
 	{
 		if (((i % 5) == 0) || ((i % 3) == 0))
@@ -20,7 +18,6 @@ int main(void)
 	printf("%d\n", n);
 	return (0);
 }
-
 
 
 
