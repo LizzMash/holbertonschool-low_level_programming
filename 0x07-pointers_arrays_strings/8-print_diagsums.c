8851b2d671e266530bd600ef43e2ad0996aa6f7c
LizzMash <elizabethmashilo@gmail.com>
2022-02-21 14:12:44 +0200

    commit functions

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,29 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_diagsums - Print sum of the two diagonals
- * of a squre matrix of intergers
- * @a: Array to use
- * @size: Size of diagonal
  * print_diagsums - prints diagonal summaries
  * @a: array to use
  * @size: size of diagonal
  */
 void print_diagsums(int *a, int size)
 {
-int i;
-int diagO = 0;
-int diagT = 0;
 	int i;
 	int diagO = 0;
 	int diagT = 0;
 
-for (i = 0; i < size; i++)
-  {
-    diagO += a[(i * size) + i];
-    diagT += a[(size - 1) + ((size - 1) * i)];
-  }
- printf("%d, %d\n", diagO, diagT);
 	for (i = 0; i < size; i++)
 	{
 		diagO += a[(i * size) + i];
