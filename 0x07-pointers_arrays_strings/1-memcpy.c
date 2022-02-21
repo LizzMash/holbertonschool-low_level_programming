0521cc3c7cf048132a741d3b91a29d960eda7064
LizzMash <elizabethmashilo@gmail.com>
2022-02-21 13:16:58 +0200

    commit functions

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /*
 * 
-_memcpy -a function that copies memory area
+*_memcpy -a function that copies memory area
 *@dest: memory where  it is stored
 *@src: memory where it is copied
 *@n: number of bytes
-*return: copied memory with n bytes changed]]=
+*return: copied memory with n bytes changed*/
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
