ae36fd8270bfd075b77a2f858cbc1dbaced4cdfe
LizzMash <elizabethmashilo@gmail.com>
2022-03-07 15:08:07 +0200

    commit preprocessor here

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,7 @@
-#ifndef MACRO_ABS
-#define MACRO_ABS
-#define ABS(x) ((x) < 0 ? -(x) : (x))
-#endi#ifndef MACRO_ABS
-#define MACRO_ABS
-#define ABS(x) ((x) < 0 ? -(x) : (x))
-#endiff
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+
+#define ABS(x) ((x) < (0) ? ((x) * (-1)): (x))
+
+#endif
